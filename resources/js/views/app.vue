13b974ed2db6c883079369494295c59d13bed6cb
urlastway <andriy.semenchenko13@gmail.com>
2022-08-02 14:04:18 +0200

    aggiungo axios

--- resources/js/views/app.vue
+++ resources/js/views/app.vue
@@ -5,10 +5,25 @@
 </template>
 
 <script>
-export default{
-    name: 'App'
-}
 
+export default{
+    name: 'App',
+    data(){
+        return {
+            posts: []
+        };
+    },
+    created(){
+        axios.get('http://127.0.0.1:8000/api/posts')
+            .then((respone)=>{
+                console.log(respone.data);
+            })
+            .catch((e)=>{
+                console.log(e);
+            });
+    },
+
+}
 </script>
 
 <style>
